72a4a68a85286f86250fb7e4c978e9cf22255864
Florian Kate <s51541@bht-berlin.de>
2023-12-08 23:09:27 +0100

    day8 done

---
+++ day8/part1.py
@@ -0,0 +1,28 @@
+def main():
+    textfile = 'input.txt'
+    with open(textfile, 'r') as file:
+        lines = file.readlines()
+    instructions = [x for x in lines[0] if x != '\n']
+    nodes = {}
+    for i in range(2, len(lines)):
+        nodes[lines[i][0:3]] = [lines[i][7:10], lines[i][12:15]]
+
+    steps = 0
+    node = 'AAA'
+    while node != 'ZZZ':
+        node = walk_sequence(instructions, nodes, node)
+        steps += len(instructions)
+    print('steps: ' + str(steps))
+
+
+def walk_sequence(instructions, nodes, node):
+    for i in range(len(instructions)):
+        if instructions[i] == 'L':
+            node = nodes[node][0]
+        else:
+            node = nodes[node][1]
+    return node
+
+
+if __name__ == '__main__':
+    main()
---
+++ day8/part2.py
@@ -0,0 +1,36 @@
+import math
+
+def main():
+    textfile = 'input.txt'
+    with open(textfile, 'r') as file:
+        lines = file.readlines()
+    instructions = [x for x in lines[0] if x != '\n']
+    nodes = {}
+    for i in range(2, len(lines)):
+        nodes[lines[i][0:3]] = [lines[i][7:10], lines[i][12:15]]
+
+    positions = [x for x in nodes if x[2] == 'A']
+    print('starting points: ' + str(positions))
+    results = []
+    for node in positions:
+        steps = 0
+        while node[2] != 'Z':
+            node = walk_sequence(instructions, nodes, node)
+            steps += len(instructions)
+        results.append(steps)
+    print('results: ' + str(results))
+    lcm = math.lcm(*results)
+    print('steps: ' + str(lcm))
+
+
+def walk_sequence(instructions, nodes, node):
+    for i in range(len(instructions)):
+        if instructions[i] == 'L':
+            node = nodes[node][0]
+        else:
+            node = nodes[node][1]
+    return node
+
+
+if __name__ == '__main__':
+    main()
